c019e2f50926cae4f14e2944c70fff5afa71a255
Onur Altuğ <onur7altug@gmail.com>
2024-03-18 22:40:46 +0300

    add changeLocation method

--- ZooProject/src/zoo/area/Location.java
+++ ZooProject/src/zoo/area/Location.java
@@ -6,11 +6,11 @@ public class Location {
 	
 	private int x;
 	private int y;
+	Random random = new Random();
 	
 	public Location() {
 		boolean findEmptyPlace = false;
 		while(!findEmptyPlace) {
-			Random random = new Random();
 			int tempX = random.nextInt(500);
 			int tempY = random.nextInt(500);
 			if(Area.getZoo()[tempX][tempY] == null) {
@@ -35,6 +35,24 @@ public class Location {
 
 	public void setY(int columnPosition) {
 		this.y = columnPosition;
+	}
+	
+	public void changeLocation(int step) {
+		int negativeStep = -1 * step;
+        int newX = random.nextInt((step - negativeStep) + 1) + negativeStep;
+        int newY = step - (Math.abs(newX));
+        if(checkLocation(getX() + newX, getY() + newY)) {
+        	Area.getZoo()[getX()][getY()] = null;
+        	setX(getX() + newX);
+        	setY(getY() + newY);
+        }
+        else {
+        	changeLocation(step);
+        }
+	}
+
+	private boolean checkLocation(int newX, int newY) {
+		return newX >= 0 && newX < 500 && newY >= 0 && newY < 500;
 	}
 
 	@Override
