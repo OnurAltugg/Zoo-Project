cde6cb44ed40215e287dbdaa01c06fa704a9e511
Onur Altuğ <onur7altug@gmail.com>
2024-03-19 03:31:31 +0300

    add animal abstract class and methods

---
+++ ZooProject/src/zoo/model/Animal.java
@@ -0,0 +1,80 @@
+package zoo.model;
+
+import zoo.area.Area;
+
+public abstract class Animal extends Creature{
+	
+	protected abstract Animal createNewAnimal();
+	
+	protected void checkCouple(Class<?> animal) {
+		int currentRow = getLocation().getX();
+		int currentColumn = getLocation().getY();
+		int maxDistance = 3;
+		
+		int startRow = Math.max(Math.abs(currentRow - maxDistance), 0); 
+		int startColumn = Math.max(Math.abs(currentColumn - maxDistance), 0);
+		int finishRow = Math.min(currentRow + maxDistance, 500);
+		int finishColumn = Math.min(currentColumn + maxDistance, 500);
+		
+		for(int x = startRow; x < finishRow; x++) {
+			for(int y = startColumn; y < finishColumn; y++) {
+				if(
+						Math.abs(currentRow - x) + Math.abs(currentColumn - y) <= maxDistance 
+						&& animal.isInstance(Area.getZoo()[x][y])
+						&& getGender() != Area.getZoo()[x][y].getGender()) {
+					System.out.println("SEX OLDU");
+					Area.addCreature(createNewAnimal());
+					return;
+				}
+				else {
+					continue;
+				}
+			}
+		}
+	}
+	
+	protected void checkHunt(Class<?> animal, int maxDistance) {
+	    int currentRow = getLocation().getX();
+	    int currentColumn = getLocation().getY();
+	    
+	    int startRow = Math.max(Math.abs(currentRow - maxDistance), 0); 
+		int startColumn = Math.max(Math.abs(currentColumn - maxDistance), 0);
+		int finishRow = Math.min(currentRow + maxDistance, 500);
+		int finishColumn = Math.min(currentColumn + maxDistance, 500);
+		
+	    for (int x = startRow; x < finishRow; x++) {
+	        for (int y = startColumn; y < finishColumn; y++) {
+	            if (Math.abs(currentRow - x) + Math.abs(currentColumn - y) <= maxDistance) {
+	                if (animal.isInstance(Wolf.class)) {
+	                    if (Area.getZoo()[x][y] instanceof Sheep || 
+	                        Area.getZoo()[x][y] instanceof Chicken || 
+	                        Area.getZoo()[x][y] instanceof Rooster) {
+	                    	Area.removeCreature(Area.getZoo()[x][y]);
+	                        Area.getZoo()[x][y] = null;
+	                        System.out.println("ADAM ÖLDÜ AQ");
+	                        return;
+	                    }
+	                } 
+	                else if (animal.isInstance(Lion.class)) {
+	                    if (Area.getZoo()[x][y] instanceof Cow || 
+	                        Area.getZoo()[x][y] instanceof Sheep) {
+	                    	Area.removeCreature(Area.getZoo()[x][y]);
+	                    	Area.getZoo()[x][y] = null;
+	                    	 System.out.println("ADAM ÖLDÜ AQ");
+	                        return;
+	                    }
+	                }
+	                else if (animal.isInstance(HunterHuman.class)) {
+	                	Area.removeCreature(Area.getZoo()[x][y]);
+	                	Area.getZoo()[x][y] = null;
+	                	 System.out.println("ADAM ÖLDÜ AQ");
+                        return;
+	                }
+	                else {
+	                	continue;
+	                }
+	            }
+	        }
+	    }
+	}
+}
