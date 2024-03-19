fa6b04a4de745170735d54cec77a8212f9c05f15
Onur Altuğ <onur7altug@gmail.com>
2024-03-19 21:35:24 +0300

    nothing changed, just editing

--- ZooProject/src/zoo/area/Area.java
+++ ZooProject/src/zoo/area/Area.java
@@ -9,18 +9,17 @@ public class Area {
 	
 	private static List<Creature> creatures = new ArrayList<>();
 	private static Creature[][] zoo = new Creature[500][500];
-	
 
 	public static List<Creature> getCreatures() {
 		return creatures;
 	}
-	
+
 	public static void addCreature(Creature creature) {
-		creatures.add(creature);
+	    creatures.add(creature);
 	}
-	
+
 	public static void removeCreature(Creature creature) {
-		creatures.remove(creature);
+	    creatures.remove(creature);
 	}
 	
 	public static Creature[][] getZoo() {
